014d67e982defc08c1f27f86d192cca994baea6f
farshed <faisalarshed28@gmail.com>
2026-08-29 17:11:57 +0500

    declutter

--- tooling/xtask/src/tasks.rs
+++ tooling/xtask/src/tasks.rs
@@ -1,9 +1,7 @@
 pub mod clippy;
-pub mod compliance;
 pub mod licenses;
 pub mod package_conformity;
 pub mod publish_gpui;
-pub mod sandbox_tests;
 pub mod setup_webrtc;
 pub mod web_examples;
 pub mod workflow_checks;
